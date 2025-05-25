38151966b70281de0f0181b5dcd742c3398f01f6
SRB <souhardaroybarman05@gmail.com>
2025-05-25 20:50:45 +0530

    too soon

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -20,7 +20,7 @@ router.post("/upload-image",upload.single("image"), (req,res) => {
   if(!req.file) {
     return res.status(400).json({message:"No file uploaded"});
   }
-  const imageUrl = `${req.protocol}://${req.get(process.env.API_URL)}/uploads/${req.file.filename}`;
+  const imageUrl = `${process.env.API_URL}/uploads/${req.file.filename}`;
   res.status(200).json({imageUrl});
 });
 
