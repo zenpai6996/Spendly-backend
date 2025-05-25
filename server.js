08fe405c2d543b7d7e478a9c8665797d672df47b
SRB <souhardaroybarman05@gmail.com>
2025-05-25 19:12:04 +0530

    feat: refine CORS configuration for improved security and functionality

--- server.js
+++ server.js
@@ -11,19 +11,36 @@ const dashboardRoutes = require("./routes/dashboardRoutes");
 
 const app = express();
 
+//database connection
+connectDB();
 
+//middlewares
 app.use(express.json());
 
+// CORS Configuration
 app.use(
   cors({
-    origin: [ "http://localhost:5173","spendly-frontend-pi.vercel.app"],
-    methods:["GET","POST","PUT","DELETE"],
-    allowedHeaders:["Content-Type","Authorization"],
+    origin: [
+      "http://localhost:5173",
+      "https://spendly-frontend-pi.vercel.app"  
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],  
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
   })
 );
 
-connectDB();
+// app.options('*', cors());
 
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'https://spendly-frontend-pi.vercel.app');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   next();
+// });
+
+//Routes
 app.use("/api/v1/auth",authRoutes);
 app.use("/api/v1/income",incomeRoutes);
 app.use("/api/v1/expense",expenseRoutes);
